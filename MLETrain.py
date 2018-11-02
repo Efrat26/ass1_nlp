6b83e14024f577302fcdf1ac4fc27227a4be20c2
efrat <efisofer26@gmail.com>
2018-11-02 13:06:14 +0200

    added "other" key to the dictionary to save the words that don't have a proper tag. also organized a bit and added comments to the code before calculating the q values.

--- MLETrain.py
+++ MLETrain.py
@@ -3,13 +3,19 @@
 
 
 def computeQE(input_file_name, q_fileName, e_fileName):
+    ####calculate e values
     listOfPossiblePP = {'NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'WP', 'VB', 'VBD', 'VBG', 'VBN', 'VBZ', 'VBP', 'MD', 'TO',
                         'JJ', 'JJR', 'JJS', 'RB', 'RBR','RBS', 'IN', 'WDT', 'DT', 'CC', 'RP', 'PRP$', 'POS', 'WRB',
                         'CD', 'PDT', 'FW', 'EX', 'SYM', 'LS', 'PDT', 'WP$', 'UH', '#', '.', ')', '(', '$', ',', ':',
-                        '``', "''"}
-    #create dictionary for the e values
-    e_dict = {}
-    pp_e_dict={}
+                        '``', "''", 'OTHER'}
+    #calculate e value, example: e(book|NN) = count(book,NN)\count(NN) and
+    #calculate q values: q(c|a,b) = L1*count(a,b,c)/count(a,b) + L2*count(b,c)/count(b) + L3*count(c)/(num of words)
+    e_dict = {} #for each pair (i.e in the example the numerator)
+    pp_e_dict={}#for each pair (i.e in the example the denominator)
+    pp_e_dict['OTHER'] = 0
+    #strings for PP that are not in the list
+    strForOtherStringsTags = "";
+    strForOtherStrings = "";
     #read from file
     with open (input_file_name) as f:
         for line in f:
@@ -26,7 +32,14 @@ def computeQE(input_file_name, q_fileName, e_fileName):
                 elif splitted_pair[-1] in listOfPossiblePP:
                     pp_e_dict[splitted_pair[-1]] = 1
                 else:
-                    print splitted_pair[-1]
+                    strForOtherStringsTags += ',' + splitted_pair[-1]
+                    strForOtherStrings += ',' + splitted_pair[0];
+                    pp_e_dict['OTHER'] += 1
+
+    numOfWords = sum(value for key, value in pp_e_dict.iteritems())
+    print numOfWords
+
+
 
     print "hello"
 
