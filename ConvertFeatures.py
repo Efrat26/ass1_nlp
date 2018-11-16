54a2be07616ae08cd19b68bedf8b9ce543f459c9
efrat <efisofer26@gmail.com>
2018-11-16 13:50:52 +0200

    created the files with the features vector and the features mapping

--- ConvertFeatures.py
+++ ConvertFeatures.py
@@ -2,13 +2,13 @@
 STUDENT={'name': 'Efrat Sofer',
          'ID': '304855125'}
 
-def FeatureConversion(feature_file_name, feature_numbers_file, labels_number_file):
+def ConvertFeatures(feature_file_name, feature_vector_file, feature_map_file):
     print ''
 
 
 
 def main():
-    FeatureConversion('extracted features', 'features number', '')
+    ConvertFeatures('extracted features', 'features number', '')
 
 if __name__ == "__main__":
     main()
--- ExtractFeatures.py
+++ ExtractFeatures.py
@@ -4,6 +4,8 @@ STUDENT={'name': 'Efrat Sofer',
          'ID': '304855125'}
 
 def ExtractFeatures(input_file, feature_file_name):
+    tags_set = set()
+    feature_set = set()
     f_input = open(input_file, 'r')
     lines = f_input.read().split('\n')
     rare_words = MLETrain.getRareWords(lines)
@@ -19,28 +21,36 @@ def ExtractFeatures(input_file, feature_file_name):
             splitted_word = word.rpartition('/')
             tags.append(splitted_word[-1])
             words.append(splitted_word [0])
+            if splitted_word[-1] not in tags_set and splitted_word[-1] != '':
+                tags_set.add(splitted_word[-1])
         for j in range(0, len(words)):
             event = ''
             if words[j] not in rare_words:
-                event += handleNotRareWords(words[j],tags[j])
+                res_event = handleNotRareWords(words[j],tags[j], feature_set)
+                event += res_event
             else:
-                event += handleRareWords(words[j],tags[j])
-            event += addEventsForEveryWords(words, tags, j)
+                res_event= handleRareWords(words[j],tags[j], feature_set)
+                event += res_event
+
+            event_res = addEventsForEveryWords(words, tags, j, feature_set)
+            event += event_res
             features.append(event)
         feature_dict[i] = features
 
     print 'writing features to file'
-    writeToFile(feature_file_name, feature_dict)
+    writeToFile(feature_file_name, feature_dict, feature_set, tags_set)
 
 
-def handleNotRareWords(word,tag):
-    event = ' word='+word +' tag='+tag
+def handleNotRareWords(word, tag, features_set):
+    event = tag + ' word='+word
+    features_set.add('word=' + word)
     return event
 
-def handleRareWords(word, tag):
-    suffix =set()
-    prefix=set()
+def handleRareWords(word, tag, features_set):
+    suffix = set()
+    prefix = set()
     event = ''
+    event += tag
     if len(word) >= 4:
         prefix.add(word[0])
         prefix.add(word[0:2])
@@ -66,47 +76,75 @@ def handleRareWords(word, tag):
             suffix.add(word[-1])
             suffix.add(word[-2:])
             suffix.add(word)
-
-    event += ' prefix:'
     for p in prefix:
-            event+=p+';'
-    event += ' suffix:'
+        event += ' prefix:'+p
+        features_set.add('prefix:'+p)
     for s in suffix:
-        event += s+';'
+        event += ' suffix:'+s
+        features_set.add('suffix:' + s)
     if any(c.isdigit() for c in word):
         event += ' containsDigit=' + 'true'
+        features_set.add('containsDigit=true')
+    else:
+        event += ' containsDigit=' + 'false'
+        features_set.add('containsDigit=false')
     if '-' in word:
         event += ' containsHyphen=' + 'true'
+        features_set.add('containsHyphen=true')
+    else:
+        event += ' containsHyphen=' + 'false'
+        features_set.add('containsHyphen=false')
     if any(c.isupper() for c in word):
         event += ' containsUpperCase='+'true'
-    event += ' tag='+tag
+        features_set.add('containsUpperCase=true')
+    else:
+        event += ' containsUpperCase=' + 'false'
+        features_set.add('containsUpperCase=false')
     return event
 
 
-def addEventsForEveryWords(words, tags, word_index):
+def addEventsForEveryWords(words, tags, word_index, features_set):
     event = ''
     if word_index > 0:
         event += ' pt=' + tags[word_index-1]
         event += ' pw='+words[word_index-1]
+        features_set.add('pt=' + tags[word_index-1])
+        features_set.add('pw=' + words[word_index - 1])
     if word_index > 1:
         event += ' ppt_pt=' + tags[word_index-2] +';'+tags[word_index-1]
         event += ' ppw=' + tags[word_index-2]
+        features_set.add('ppt_pt=' + tags[word_index-2] +';'+tags[word_index-1])
+        features_set.add('ppw='+ tags[word_index-2])
     if word_index <len(words)-1:
         event += ' nw='+words[word_index+1]
+        features_set.add('nw='+words[word_index+1])
     if word_index < len(words)-2:
         event += ' nnw='+words[word_index+2]
-
+        features_set.add('nnw='+words[word_index+2])
     return event
 
-def writeToFile(output_file, output_dict):
+def writeToFile(feature_file, feature_lines_dict,  feature_set, tags):
     written_lines = {}
-    output_file = open(output_file, 'w')
-    for key in output_dict:
-        features = output_dict[key]
+    counter = 1
+
+    feature_lines_output_file = open(feature_file, 'w')
+    feature_vec_file = open('feature vec file', 'w')
+    feature_map_file = open('feature map file', 'w')
+    for key in feature_lines_dict:
+        features = feature_lines_dict[key]
         for feature_line in features:
             if feature_line not in written_lines:
-                output_file.write(feature_line + '\n')
+                feature_lines_output_file.write(feature_line + '\n')
                 written_lines[feature_line] = 1
+
+    for tag in tags:
+        feature_vec_file.write(tag+'\n')
+        feature_map_file.write(tag + ' ' + str(counter) + '\n')
+        counter += 1
+    for feature in feature_set:
+        feature_vec_file.write(feature + '\n')
+        feature_map_file.write(feature + ' ' + str(counter) + '\n')
+        counter += 1
 
 
 
