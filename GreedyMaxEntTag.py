e9a4e74d39763aae59a4044b02d1fd1fd5b1a4e8
Efrat26 <35764255+Efrat26@users.noreply.github.com>
2018-11-18 20:45:23 +0200

    continued working on greedy max ent

--- GreedyMaxEntTag.py
+++ GreedyMaxEntTag.py
@@ -14,6 +14,7 @@ def GreedyMaxEntTag(input_file_name, model_name, feature_map_file, out_file_name
     # read the feature mapping
     feature_mapping_file = open(feature_map_file, 'r')
     features_dict = {}
+    features_dict_number_is_ind = {}
     # open the file and extract all values
     feature_mapping_lines = feature_mapping_file.read().split('\n')
     for line in feature_mapping_lines:
@@ -22,10 +23,11 @@ def GreedyMaxEntTag(input_file_name, model_name, feature_map_file, out_file_name
             if len(splitted_line) != 2:
                 # print 'length of splitted line isnt 2, line: ' + line
                 continue
-            features_dict[splitted_line[0]] = splitted_line[1]
+            features_dict[splitted_line[0]] = int(splitted_line[1])
+            features_dict_number_is_ind[int(splitted_line[1])] = splitted_line[0]
 
     #get list of tags
-    list_of_tags = ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'WP', 'VB', 'VBD', 'VBG', 'VBN', 'VBZ', 'VBP', 'MD', 'TO',
+    list_of_tags = ['NNS', 'NN', 'NNP', 'NNPS', 'PRP', 'WP', 'VB', 'VBD', 'VBG', 'VBN', 'VBZ', 'VBP', 'MD', 'TO',
                     'JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS', 'IN', 'WDT', 'DT', 'CC', 'RP', 'PRP$', 'POS', 'WRB',
                     'CD', 'PDT', 'FW', 'EX', 'SYM', 'LS', 'WP$', 'UH', '#', '.', ')', '(', '$', ',', ':',
                     '``', "''"]
@@ -53,25 +55,17 @@ def GreedyMaxEntTag(input_file_name, model_name, feature_map_file, out_file_name
         for j in range(0, len(words)):
             max_result = 0.0
             max_label = None
-            for tag in list_of_tags:
-                #extract features for the word:
-                event = ''
-                if words[j] in rare_words:
-                    event += ExtractFeatures.handleRareWords(words[j], '', set())
-                else:
-                    event += ExtractFeatures.handleNotRareWords(words[j], '', set())
-                event += ExtractFeatures.addEventsForEveryWords(words,tags , j, set())
-                #convert features to numbers according to feature map
-                converted_feature = ConvertFeatures.ConvertFeaturesFromSentence(event, feature_map_file, tag, features_dict)
-                #insert to the trained model
-                result = loaded_model.predict([converted_feature])
-                print result
-                if result > max_result:
-                    max_result = result
-                    max_label = tag
-                #check if it's bigger than
-                #print 'hello'
-            tags.insert(j, max_label)
+            # extract features for the word:
+            event = ''
+            if words[j] in rare_words:
+                event += ExtractFeatures.handleRareWords(words[j], '', set())
+            else:
+                event += ExtractFeatures.handleNotRareWords(words[j], '', set())
+            event += ExtractFeatures.addEventsForEveryWords(words, tags, j, set())
+            converted_feature = ConvertFeatures.ConvertFeaturesFromSentence(event, features_dict)
+            result = loaded_model.predict([converted_feature])
+            #print str(result)
+            tags.insert(j, str(features_dict_number_is_ind[int(result[0])]))
 
         sentence = createSentence(words[2:], tags[2:])
         result_dict[i] = sentence
@@ -97,6 +91,7 @@ def writeToOutputFile(result_dict, len_lines, out_file_name):
 
 def main():
     GreedyMaxEntTag('/home/efrat/Documents/nlp/ass1/data/ass1-tagger-test-input', 'model_file', 'feature map file', 'output_greedy_memm')
+    print 'calculating accuracy'
     GreedyTag.calculateAccuracy('output_greedy_memm', '/home/efrat/Documents/nlp/ass1/data/ass1-tagger-test')
 
 
